db55c1bfb26d8d906f180c127b72f587fb6683e3
Daniel Bauer <103129872+Shrike717@users.noreply.github.com>
2023-12-16 12:34:54 +0100

    Delete 02. Authentication-Next - NextAuth-Prisma-Postgres - Medium/authapp directory

--- 02. Authentication-Next - NextAuth-Prisma-Postgres - Medium/authapp/src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        HOME
-      </div>
-    </main>
-  );
-}
